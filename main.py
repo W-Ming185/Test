1a8bc03cce86a5686ef55a22385c8a8106da8796
WoodSudlor <supakrit.wpn@gmail.com>
2026-05-25 23:35:04 +0700

    Add function2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import Function1
+import Function2
 
 def main():
     A = 1
     B = 2
-    print(f"{Function1.sum_of_2(A , B)}")
+    print(f"Sum is {Function1.sum_of_2(A , B)} Multiply is {Function2.multiply(A , B)}")
 
 if __name__ == "__main__":
     main()
