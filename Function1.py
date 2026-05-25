7a4ab6cc2ce9994f80e4faa60621d80882a4c1cd
WoodSudlor <supakrit.wpn@gmail.com>
2026-05-25 19:05:55 +0700

    init main and added Function1

---
+++ Function1.py
@@ -0,0 +1,2 @@
+def sum_of_2(a , b):
+    return a + b
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import Function1
 
+def main():
+    A = 1
+    B = 2
+    print(f"{Function1.sum_of_2(A , B)}")
+
+if __name__ == "__main__":
+    main()
